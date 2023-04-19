a5bde6b64839f367aeacb3e16dd8efae9551721f
2019076880 <2019076880@hanyang.ac.kr>
2023-04-19 04:07:57 +0000

    unit test

--- xv6-public/test_mlfq_unit.c
+++ xv6-public/test_mlfq_unit.c
@@ -5,28 +5,77 @@
 int
 main(int argc, char *argv[])
 {
-    printf(1, "***** yield test *****\n");
     int pid;
 
+    printf(1, "***** yield test *****\n");
     pid = fork();
     for(int i = 0; i < 10; i++){
         if(pid == 0){
-            printf(1, "Child\n");
+            if(i == 0)
+                printf(1, "Child\n");
             yield();
         }
         else{
-            printf(1,"Parent\n");
+            if(i == 0)
+                printf(1,"Parent\n");
             yield();
+            while (wait() != -1)
+                sleep(1);
         }
     }
-    if (pid > 0)
-        wait(); // 자식 프로세스가 zombie 되지 않도록 wait()
 
     printf(1, "***** getLevel test *****\n");
+    pid = fork();
+    for(int i = 0; i < 10; i++){
+        if(pid == 0){
+            if(i == 0)
+                printf(1, "Child\n");
+            printf(1, "level: %d\n", getLevel());
+        }
+        else{
+            if(i == 0)
+                printf(1, "Parent\n");
+            printf(1, "level: %d\n", getLevel());
+            while (wait() != -1)
+                sleep(1);
+        }
+    }
 
     printf(1, "***** setPriority test *****\n");
+    pid = fork();
+    for(int i = 0; i < 10; i++){
+        if(pid == 0){
+            if(i == 0){
+                printf(1, "Child\n");
+                setPriority(getpid(), 0);
+            }
+        }
+        else{
+            if(i == 0)
+                printf(1,"Parent\n");
+            while (wait() != -1)
+                sleep(1);
+        }
+    }
     
     printf(1, "***** schedulerLock/Unlock test *****\n");
-    
+    pid = fork();
+    for(int i = 0; i < 100; i++){
+        if(pid == 0){
+            if(i == 0){
+                printf(1, "Child\n");
+                schedulerLock(2019076880);
+            }
+            if(i == 99)
+                schedulerUnlock(2019076880);
+        }
+        else{
+            if(i == 0)
+                printf(1,"Parent\n");
+            while (wait() != -1)
+                sleep(1);
+        }
+    }
+
     exit();
 }
