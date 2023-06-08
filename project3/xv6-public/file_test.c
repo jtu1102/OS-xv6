9ab1a2afe7181aec14bb64d8e7d71eeeca3295e5
2019076880 <2019076880@hanyang.ac.kr>
2023-06-08 13:04:35 +0000

    test code

--- project3/xv6-public/file_test.c
+++ project3/xv6-public/file_test.c
@@ -4,18 +4,16 @@
 #include "fcntl.h"
 
 #define BSIZE 512
-#define NINDIRECT (BSIZE / sizeof(uint))
-#define NDBLINDIRECT ((NINDIRECT) * (NINDIRECT))
-#define NTRPLDIRECT ((NINDIRECT) * (NINDIRECT) * (NINDIRECT))
-// #define NUM_BYTES ((NDBLINDIRECT + 10000) * (BSIZE)) // 13.5MB
-#define NUM_BYTES ((1000) * (BSIZE))
+#define NINDIRECT (BSIZE / sizeof(uint)) // 128
+#define NDBLINDIRECT ((NINDIRECT) * (NINDIRECT)) // 16384
+#define NTRPLDIRECT ((NINDIRECT) * (NINDIRECT) * (NINDIRECT)) // 2097152
+#define NUM_BYTES (NINDIRECT * BSIZE) // 65KB
+#define NUM_BYTES1 ((NINDIRECT + 10000) * (BSIZE)) // 5MB
+#define NUM_BYTES2 ((NDBLINDIRECT + 10000) * (BSIZE)) // 13.5MB
 #define NUM_TRPL_BYTES ((NTRPLDIRECT) * (BSIZE))
 
-#define NUM_TEST3 10
-
-char buf[NUM_BYTES], buf2[NUM_BYTES];
-char filename[16] = "test_file0";
-const int len = 10;
+char buf[NUM_BYTES], buf1[NUM_BYTES1], buf2[NUM_BYTES2];
+char filename[16] = "test.txt";
 
 void failed(const char *msg)
 {
@@ -24,12 +22,11 @@ void failed(const char *msg)
   exit();
 }
 
-void test1(int first)
+void test1()
 {
   int fd;
 
-  if (first)
-    printf(1, "Test 1: Write %d bytes\n", NUM_BYTES);
+  printf(1, "Test 1: Write %d bytes\n", NUM_BYTES);
   fd = open(filename, O_CREATE | O_WRONLY);
   if (fd < 0)
     failed("File open error\n");
@@ -37,37 +34,37 @@ void test1(int first)
     failed("File write error\n");
   if (close(fd) < 0)
     failed("File close error\n");
-  if (first)
-    printf(1, "Test 1 passed\n\n");
+  printf(1, "Test 1 passed\n\n");
 }
 
-void test2(int first)
+void test2()
 {
-  int i, fd;
-  for (i = 0; i < NUM_BYTES; i++)
-    buf2[i] = 0;
-  
-  if (first)
-    printf(1, "Test 2: Read %d bytes\n", NUM_BYTES);
-  fd = open(filename, O_RDONLY);
+  int fd;
+
+  printf(1, "Test 2: Write %d bytes\n", NUM_BYTES1);
+  fd = open(filename, O_CREATE | O_WRONLY);
   if (fd < 0)
     failed("File open error\n");
-  if (read(fd, buf2, NUM_BYTES) < 0)
-    failed("File read error\n");
-  for (i = 0; i < NUM_BYTES; i++) {
-    if (buf2[i] != (i % 26) + 'a') {
-      printf(1, "%dth character, expected %c, found %c\n", i, (i % 26) + 'a', buf2[i]);
-      failed("");
-    }
-  }
+  if (write(fd, buf1, NUM_BYTES1) < 0)
+    failed("File write error\n");
   if (close(fd) < 0)
     failed("File close error\n");
-  if (unlink(filename) < 0)
-    failed("File unlink error\n");
-  if ((fd = open(filename, O_RDONLY)) >= 0)
-      failed("File not erased\n");
-  if (first)
-    printf(1, "Test 2 passed\n\n");
+  printf(1, "Test 2 passed\n\n");
+}
+
+void test3()
+{
+  int fd;
+
+  printf(1, "Test 3: Write %d bytes\n", NUM_BYTES2);
+  fd = open(filename, O_CREATE | O_WRONLY);
+  if (fd < 0)
+    failed("File open error\n");
+  if (write(fd, buf2, NUM_BYTES2) < 0)
+    failed("File write error\n");
+  if (close(fd) < 0)
+    failed("File close error\n");
+  printf(1, "Test 3 passed\n\n");
 }
 
 int main(int argc, char *argv[])
@@ -75,22 +72,14 @@ int main(int argc, char *argv[])
   int i;
   for (i = 0; i < NUM_BYTES; i++)
     buf[i] = (i % 26) + 'a';
+  for (i = 0; i < NUM_BYTES1; i++)
+    buf1[i] = (i % 26) + 'a';
+  for (i = 0; i < NUM_BYTES2; i++)
+    buf2[i] = (i % 26) + 'a';
 
-  test1(1);
-  // test2(1);
-
-  // printf(1, "Test 3: repeating test 1 & 2\n");
-  // for (i = 0; i < NUM_TEST3; i++)
-  // {
-  //   printf(1, "Loop %d: ", i + 1);
-  //   filename[len - 1] = (i % 10) + '0';
-  //   printf(1, "1.. ");
-  //   test1(0);
-  //   printf(1, "2.. ");
-  //   test2(0);
-  //   printf(1, "ok\n");
-  // }
-  // printf(1, "Test 3 passed\n");
+  test1();
+  test2();
+  test3();
   
   printf(1, "All tests passed!!\n");
   exit();
